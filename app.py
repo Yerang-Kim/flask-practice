811419ca89f210f9aaaf116a748d67882d083320
yerang <grondin@naver.com>
2021-10-07 19:57:13 +0900

    connect database, add simple template

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from enum import unique
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMINT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
-    return 'Hello, Flask!'
+    return render_template('hello.html')
